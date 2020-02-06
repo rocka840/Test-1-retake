9628cdb122552cc8edf0fd7321ca1b0251f0cc8b
rocka840 <you@example.com>
2020-02-06 10:53:16 +0100

    :)

---
+++ EXERCISE THREE/three.js
@@ -0,0 +1,13 @@
+let Q = prompt("What is 50 divided by 10?");
+
+for (const i = 0; i >= 10; i++) {
+
+    if (Q != 5) {
+        prompt("What is 50 divided by 10?");
+    }
+    if (Q == 5) {
+        alert("Congrats - you know math!");
+    }
+
+    alert("I give-up! You definitely have some problems!");
+}
---
+++ EXERCISE TWO/two.js
@@ -0,0 +1,33 @@
+var  x  =  0;
+
+var  y  =  1;
+
+var  m  =  0;
+
+if  (m  =  1) {
+    console.log ("m is NOT 0");
+}
+
+    
+
+
+
+for  (i  ==  0;  x < 1000000;  i++) {
+
+        
+    console.log(x);
+
+        
+    m  =  x  +  y;
+
+        
+    x  =  y;
+
+        
+    y  =  m;
+
+}
+
+i = 1;
+
+console.log("There are + i + " + " fibonacci numbers");
